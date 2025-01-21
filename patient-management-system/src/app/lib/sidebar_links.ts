da233f201ea81f7a58db75f3f336677b630fc7b4
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-01-21 16:18:29 +0530

    Added comment

--- patient-management-system/src/app/lib/sidebar_links.ts
+++ patient-management-system/src/app/lib/sidebar_links.ts
@@ -38,3 +38,27 @@ export const NurseLinks: (SideBarItemType | ExpandingSidebarItem)[] = [
         link: "/queue"
     }
 ];
+
+//You can add expanding sidebar items like this
+// {
+//     icon: MdSpaceDashboard,
+//     name: "Dashboard",
+//     initially_expanded: true,
+//     links: [
+//         {
+//             icon: MdSpaceDashboard,
+//             name: "Dashboard",
+//             link: "/dashboard"
+//         },
+//         {
+//             icon: BsFillPeopleFill,
+//             name: "Patients",
+//             link: "/patients"
+//         },
+//         {
+//             icon: FaClock,
+//             name: "Queue",
+//             link: "/queue"
+//         }
+//     ]
+// }
