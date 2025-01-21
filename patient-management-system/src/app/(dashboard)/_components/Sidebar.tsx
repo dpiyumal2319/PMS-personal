d3e05b3fbf2a1a6baca98c6f7c50bb017e2dc406
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-01-21 17:36:49 +0530

    Changes sidebar boarder colors

--- patient-management-system/src/app/(dashboard)/_components/Sidebar.tsx
+++ patient-management-system/src/app/(dashboard)/_components/Sidebar.tsx
@@ -5,7 +5,7 @@ import SideBarLinksWrapper from "@/app/(dashboard)/_components/SideBarLinksWrapp
 
 export default function Sidebar() {
     return (
-        <div className={"h-screen w-52 bg-primary-500 flex flex-col px-2 py-3 justify-start shadow-lg border-r-2 border-primary-900"}>
+        <div className={"h-screen w-52 bg-primary-500 flex flex-col px-2 py-3 justify-start shadow-lg border-r-2 border-primary-900/25"}>
             <div className={"flex justify-center items-center gap-5 py-3 mb-10"}>
                 <FaStethoscope className={"text-5xl text-white"}/>
                 <span className={"text-2xl text-white font-extrabold"}>PMS</span>
