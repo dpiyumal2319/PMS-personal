9fb79b3ee7da1f95e667b92268ba4a57bbed0501
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-02-12 16:31:23 +0530

    feat: add random patient data generation to seed script and update dependencies

--- patient-management-system/src/app/(dashboard)/inventory/cost-management/layout.tsx
+++ patient-management-system/src/app/(dashboard)/inventory/cost-management/layout.tsx
@@ -1,5 +1,6 @@
 // app/inventory/layout.tsx
 import TabsBar from "@/app/(dashboard)/inventory/cost-management/_components/TabBar";
+import React from "react";
 
 export default function InventoryLayout({
                                             children,
@@ -8,7 +9,7 @@ export default function InventoryLayout({
 }) {
     return (
         <div>
-            <div className={'h-14 bg-white flex items-center justify-between py-3 px-4 border-b border-primary-900/25 shadow sticky top-0'}>
+            <div className={'h-14 bg-white flex items-center justify-between py-3 px-4 border-b border-primary-900/25 shadow sticky top-0 z-50'}>
                 <TabsBar/>
             </div>
             <div>
