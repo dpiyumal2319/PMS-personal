e84d17a5ea635c9ebb80b9a735d85dbfcbca8219
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-02-16 03:08:30 +0530

    feat: refactor loading component; remove unused Loading function and add project configuration files

--- patient-management-system/src/app/(dashboard)/queue/loading.tsx
+++ patient-management-system/src/app/(dashboard)/queue/loading.tsx
@@ -2,13 +2,6 @@ import React from 'react';
 import {TableBody, TableCell, TableRow} from "@/components/ui/table";
 import {Skeleton} from "@/components/ui/skeleton";
 
-export function Loading() {
-    return (
-        //Random skeleton loading
-        <Skeleton className={'w-3/4 h-4'}/>
-    );
-}
-
 function LoadingRows({rows, cols}: { rows: number, cols: number }) {
     return (
         <TableBody>
