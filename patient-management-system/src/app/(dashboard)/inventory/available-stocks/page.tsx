da256253b9a7d459ecbe4241d10406d5ab63f160
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-02-17 23:19:06 +0530

    feat: add error handling component and refactor InventoryAvailable for improved readability

--- patient-management-system/src/app/(dashboard)/inventory/available-stocks/page.tsx
+++ patient-management-system/src/app/(dashboard)/inventory/available-stocks/page.tsx
@@ -9,53 +9,52 @@ import AvailableStockPageTable from "./_components/AvailableStockPageTable";
 import DrugListSkeleton from "./_components/DrugListSkeleton";
 
 export default async function InventoryAvailable({
-  searchParams,
+    searchParams,
 }: {
-  searchParams?: Promise<{
-    query?: string;
-    page?: string;
-    selection?: string;
-    sort?: string;
-  }>;
+    searchParams?: Promise<{
+        query?: string;
+        page?: string;
+        selection?: string;
+        sort?: string;
+    }>;
 }) {
-  const params = await searchParams;
-  const query = params?.query || "";
-  const currentPage = Number(params?.page) || 1;
-  const selection = params?.selection || "model";
-  const sort = params?.sort || "alphabetically";
+    const params = await searchParams;
+    const query = params?.query || "";
+    const currentPage = Number(params?.page) || 1;
+    const selection = params?.selection || "model";
+    const sort = params?.sort || "alphabetically";
 
-  return (
-    <div className="flex h-full flex-col w-full">
-      <div className="sticky top-0 p-4 bg-white border-b shadow-md flex gap-4 z-20">
-        <div className="relative w-[200px]">
-          <SearchPanel placeholder="Search by Name" />
+    return (
+        <div className="flex h-full flex-col w-full">
+            <div className="sticky top-0 p-4 bg-white border-b shadow-md flex gap-4 z-20">
+                <div className="relative w-[200px]">
+                    <SearchPanel placeholder="Search by Name" />
+                </div>
+                <Dropdown
+                    items={[
+                        { label: "By Model", value: "model" },
+                        { label: "By Brand", value: "brand" },
+                        { label: "By Batch", value: "batch" },
+                    ]}
+                    urlParameterName="selection"
+                />
+
+                <SortingDropdown selection={selection} />
+                <div>
+                    <DrugForm />
+                </div>
+            </div>
+            <div className="flex-grow overflow-auto">
+                <Suspense fallback={<DrugListSkeleton isLoading={true} />}>
+                    {" "}
+                    <AvailableStockPageTable
+                        query={query}
+                        currentPage={currentPage}
+                        selection={selection}
+                        sort={sort}
+                    />
+                </Suspense>
+            </div>
         </div>
-        <Dropdown
-          items={[
-            { label: "By Model", value: "model" },
-            { label: "By Brand", value: "brand" },
-            { label: "By Batch", value: "batch" },
-          ]}
-          urlParameterName="selection"
-        />
-
-        <SortingDropdown selection={selection} />
-        <div>
-          <DrugForm />
-        </div>
-      </div>
-      <div className="flex-grow overflow-auto">
-        <Suspense fallback={<DrugListSkeleton isLoading={true} />}>
-          {" "}
-          <AvailableStockPageTable
-            query={query}
-            currentPage={currentPage}
-            selection={selection}
-            sort={sort}
-          />
-        </Suspense>
-      </div>
-
-    </div>
-  );
+    );
 }
