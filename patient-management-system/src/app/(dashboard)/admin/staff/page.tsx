ec79af24d31d155de360d8446a4de29d74452ba0
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-02-17 16:02:10 +0530

    feat: update AdminPage and UserDetails components; improve layout and add profile link for user details

--- patient-management-system/src/app/(dashboard)/admin/staff/page.tsx
+++ patient-management-system/src/app/(dashboard)/admin/staff/page.tsx
@@ -8,13 +8,12 @@ import StaffSkeleton from "@/app/(dashboard)/admin/staff/_components/StaffCardSk
 
 export default async function AdminPage() {
     return (
-        <div className="p-4 min-h-screen">
+        <div className="p-4 h-full">
             <div className={'flex justify-between items-center mb-6'}>
                 <h1 className="text-2xl font-bold text-primary-700">
                     Medical Staff Management
                 </h1>
                 <AddUserDialog/>
-
             </div>
             <Suspense fallback={<StaffSkeleton/>}>
                 <AllUsers/>
