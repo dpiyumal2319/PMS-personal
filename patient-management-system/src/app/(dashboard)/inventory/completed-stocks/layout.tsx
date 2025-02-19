b5ce73f3bb521fe4875f2f5548c629307e7c2b99
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-02-19 12:21:15 +0530

    refactor: remove background color from layout for improved flexibility

--- patient-management-system/src/app/(dashboard)/inventory/completed-stocks/layout.tsx
+++ patient-management-system/src/app/(dashboard)/inventory/completed-stocks/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({
     children: React.ReactNode;
 }) {
     return (
-        <div className={'flex flex-col h-full bg-amber-600'}>
+        <div className={'flex flex-col h-full'}>
             <div className="flex-1 min-h-0 overflow-y-auto">
                 {children}
             </div>
