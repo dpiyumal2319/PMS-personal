f4a6ab120b7d2392f1b175867d474adaba7663fe
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-02-23 06:27:21 +0530

    feat: refactor Loading component to use Skeleton for improved loading state representation

--- patient-management-system/src/app/(dashboard)/loading.tsx
+++ patient-management-system/src/app/(dashboard)/loading.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
+import {Skeleton} from "@/components/ui/skeleton";
 
-function Loading() {
+export default function LoadingPage() {
     return (
-        <div role="status" className="max-w-sm animate-pulse">
-            <div className="h-2.5 bg-gray-200 rounded-full  w-48 mb-4"></div>
-            <div className="h-2 bg-gray-200 rounded-full  max-w-[360px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full  max-w-[330px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full  max-w-[300px] mb-2.5"></div>
-            <div className="h-2 bg-gray-200 rounded-full  max-w-[360px]"></div>
-            <span className="sr-only">Loading...</span>
+        <div className="p-6 space-y-6">
+            {/* Skeleton Cards */}
+            <div className="grid grid-cols-3 gap-4">
+                <Skeleton className="h-32 w-full"/>
+                <Skeleton className="h-32 w-full "/>
+                <Skeleton className="h-32 w-full "/>
+            </div>
+
+            {/* Skeleton Rows */}
+            <div className="space-y-4">
+                <Skeleton className="h-16 w-full"/>
+                <Skeleton className="h-16 w-full"/>
+                <Skeleton className="h-16 w-full"/>
+                <Skeleton className="h-16 w-full"/>
+                <Skeleton className="h-16 w-full"/>
+            </div>
         </div>
     );
 }
-
-export default Loading;
