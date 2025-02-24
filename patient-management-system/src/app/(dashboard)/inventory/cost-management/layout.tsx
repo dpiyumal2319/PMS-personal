9fc03f16a252d64003a50c30411261ea1efbd22c
Prashant-Pubudu <100054944+Manojpu@users.noreply.github.com>
2025-02-24 21:59:46 +0530

    refactor: improve layout and styling in Stock components for better usability

--- patient-management-system/src/app/(dashboard)/inventory/cost-management/layout.tsx
+++ patient-management-system/src/app/(dashboard)/inventory/cost-management/layout.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
+import React from "react";
 import TabsBar from "@/app/(dashboard)/inventory/cost-management/_components/TabBar";
-import {Metadata} from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "PMS - Cost Management",
-    description: "Manage and view cost management.",
+  title: "PMS - Cost Management",
+  description: "Manage and view cost management.",
 };
 
-const InventoryLayout = ({children}: { children: React.ReactNode }) => {
-    return (
-        <div className="flex flex-col h-full overflow-hidden">
-            <div className="flex-none bg-white border-b border-primary-900/25 shadow z-10  py-2 px-4">
-                <TabsBar/>
-            </div>
-            <div className="flex-1 overflow-y-auto min-h-0">
-                {children}
-            </div>
-        </div>
-    );
+const InventoryLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col h-full overflow-hidden">
+      <div className="flex-none bg-white border-b border-primary-900/25 shadow z-5  py-2 px-4">
+        <TabsBar />
+      </div>
+      <div className="flex-1 overflow-y-auto min-h-0">{children}</div>
+    </div>
+  );
 };
 
 export default InventoryLayout;
