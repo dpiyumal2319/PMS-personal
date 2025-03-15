cc8abab036301e732e7ea474d11d03132549d0bb
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-03-15 16:59:35 +0530

    feat: add revalidation for batch status updates and refactor button actions in BatchStatusChangeButtonBar

--- patient-management-system/src/app/(dashboard)/inventory/available-stocks/_components/BatchStatusChangeButtonBar.tsx
+++ patient-management-system/src/app/(dashboard)/inventory/available-stocks/_components/BatchStatusChangeButtonBar.tsx
@@ -1,29 +1,27 @@
 "use client";
 
-import { AlertDialogFooter, AlertDialogHeader } from '@/components/ui/alert-dialog';
-import { Button } from '@/components/ui/button';
-import { AlertDialog, AlertDialogContent, AlertDialogTrigger } from '@/components/ui/alert-dialog';
-import { useState } from 'react';
-import { AlertDialogTitle } from '@radix-ui/react-alert-dialog';
-import { AiOutlineCheck, AiOutlineClose } from 'react-icons/ai';
-import { IoMdDoneAll } from 'react-icons/io';
-import { FaTrashAlt } from 'react-icons/fa';
-import { MdEventAvailable, MdSmsFailed } from 'react-icons/md';
-import { handleConfirmationOfBatchStatusChange } from '@/app/lib/actions';
-import { useRouter } from 'next/navigation'; // Import useRouter
+import {AlertDialogFooter, AlertDialogHeader} from '@/components/ui/alert-dialog';
+import {Button} from '@/components/ui/button';
+import {AlertDialog, AlertDialogContent, AlertDialogTrigger} from '@/components/ui/alert-dialog';
+import {useState} from 'react';
+import {AlertDialogTitle} from '@radix-ui/react-alert-dialog';
+import {AiOutlineCheck, AiOutlineClose} from 'react-icons/ai';
+import {IoMdDoneAll} from 'react-icons/io';
+import {FaTrashAlt} from 'react-icons/fa';
+import {MdEventAvailable, MdSmsFailed} from 'react-icons/md';
+import {handleConfirmationOfBatchStatusChange} from '@/app/lib/actions';
+import {handleServerAction} from "@/app/lib/utils"; // Import useRouter
 
-const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
-    const [action, setAction] = useState<"completed" | "disposed" | "quality_failed"| "available"| null>(null);
+const BatchStatusChangeButtonBar = ({batchId}: { batchId: string }) => {
+    const [action, setAction] = useState<"completed" | "disposed" | "quality_failed" | "available" | null>(null);
     const [completedOpen, setCompletedOpen] = useState(false);
     const [qualityFailedOpen, setQualityFailedOpen] = useState(false);
     const [availableOpen, setAvailableOpen] = useState(false);
     const [disposedOpen, setDisposedOpen] = useState(false);
-    const router = useRouter(); // Initialize useRouter
 
     const handleConfirm = async () => {
         if (action) {
-            await handleConfirmationOfBatchStatusChange(Number(batchId), action);
-            router.refresh(); // Refresh the page
+            await handleServerAction(() => handleConfirmationOfBatchStatusChange(Number(batchId), action), {loadingMessage: `Marking batch as ${action}...`}); // Call handleServerAction
         }
     };
 
@@ -31,8 +29,11 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
         <div className="flex justify-center gap-4">
             <AlertDialog open={completedOpen} onOpenChange={setCompletedOpen}>
                 <AlertDialogTrigger asChild>
-                    <Button className="bg-primary hover:bg-primary-600" onClick={() => { setCompletedOpen(true); setAction("completed"); }}>
-                        <IoMdDoneAll className="mr-2" />
+                    <Button className="bg-primary hover:bg-primary-600" onClick={() => {
+                        setCompletedOpen(true);
+                        setAction("completed");
+                    }}>
+                        <IoMdDoneAll className="mr-2"/>
                         Mark as Completed
                     </Button>
                 </AlertDialogTrigger>
@@ -42,13 +43,13 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
                     </AlertDialogHeader>
                     <AlertDialogFooter>
                         <Button variant="outline" onClick={() => setCompletedOpen(false)}>
-                            <AiOutlineClose className="mr-2" /> Cancel
+                            <AiOutlineClose className="mr-2"/> Cancel
                         </Button>
                         <Button className="bg-primary hover:bg-primary-600" onClick={async () => {
                             await handleConfirm();
                             setCompletedOpen(false);
                         }}>
-                            <AiOutlineCheck className="mr-2" /> Confirm
+                            <AiOutlineCheck className="mr-2"/> Confirm
                         </Button>
                     </AlertDialogFooter>
                 </AlertDialogContent>
@@ -56,8 +57,11 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
 
             <AlertDialog open={availableOpen} onOpenChange={setAvailableOpen}>
                 <AlertDialogTrigger asChild>
-                    <Button className="bg-green-600 hover:bg-green-700" onClick={() => { setAvailableOpen(true); setAction("available"); }}>
-                        <MdEventAvailable className="mr-2" />
+                    <Button className="bg-green-600 hover:bg-green-700" onClick={() => {
+                        setAvailableOpen(true);
+                        setAction("available");
+                    }}>
+                        <MdEventAvailable className="mr-2"/>
                         Revoke
                     </Button>
                 </AlertDialogTrigger>
@@ -67,13 +71,13 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
                     </AlertDialogHeader>
                     <AlertDialogFooter>
                         <Button variant="outline" onClick={() => setAvailableOpen(false)}>
-                            <AiOutlineClose className="mr-2" /> Cancel
+                            <AiOutlineClose className="mr-2"/> Cancel
                         </Button>
                         <Button className="bg-green-600 hover:bg-green-700" onClick={async () => {
                             await handleConfirm();
                             setAvailableOpen(false);
                         }}>
-                            <AiOutlineCheck className="mr-2" /> Confirm
+                            <AiOutlineCheck className="mr-2"/> Confirm
                         </Button>
                     </AlertDialogFooter>
                 </AlertDialogContent>
@@ -82,8 +86,11 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
 
             <AlertDialog open={qualityFailedOpen} onOpenChange={setQualityFailedOpen}>
                 <AlertDialogTrigger asChild>
-                    <Button className="bg-violet-500 hover:bg-violet-600" onClick={() => { setQualityFailedOpen(true); setAction("quality_failed"); }}>
-                        <MdSmsFailed  className="mr-2" />
+                    <Button className="bg-violet-500 hover:bg-violet-600" onClick={() => {
+                        setQualityFailedOpen(true);
+                        setAction("quality_failed");
+                    }}>
+                        <MdSmsFailed className="mr-2"/>
                         Quality Failed
                     </Button>
                 </AlertDialogTrigger>
@@ -93,13 +100,13 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
                     </AlertDialogHeader>
                     <AlertDialogFooter>
                         <Button variant="outline" onClick={() => setQualityFailedOpen(false)}>
-                            <AiOutlineClose className="mr-2" /> Cancel
+                            <AiOutlineClose className="mr-2"/> Cancel
                         </Button>
                         <Button className="bg-violet-500 hover:bg-violet-600" onClick={async () => {
                             await handleConfirm();
                             setQualityFailedOpen(false);
                         }}>
-                            <AiOutlineCheck className="mr-2" /> Confirm
+                            <AiOutlineCheck className="mr-2"/> Confirm
                         </Button>
                     </AlertDialogFooter>
                 </AlertDialogContent>
@@ -107,8 +114,11 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
 
             <AlertDialog open={disposedOpen} onOpenChange={setDisposedOpen}>
                 <AlertDialogTrigger asChild>
-                    <Button variant="destructive" onClick={() => { setDisposedOpen(true); setAction("disposed"); }}>
-                        <FaTrashAlt className="mr-2" />
+                    <Button variant="destructive" onClick={() => {
+                        setDisposedOpen(true);
+                        setAction("disposed");
+                    }}>
+                        <FaTrashAlt className="mr-2"/>
                         Dispose
                     </Button>
                 </AlertDialogTrigger>
@@ -118,13 +128,13 @@ const BatchStatusChangeButtonBar = ({ batchId }: { batchId: string }) => {
                     </AlertDialogHeader>
                     <AlertDialogFooter>
                         <Button variant="outline" onClick={() => setDisposedOpen(false)}>
-                            <AiOutlineClose className="mr-2" /> Cancel
+                            <AiOutlineClose className="mr-2"/> Cancel
                         </Button>
                         <Button variant="destructive" onClick={async () => {
                             await handleConfirm();
                             setDisposedOpen(false);
                         }}>
-                            <AiOutlineCheck className="mr-2" /> Confirm
+                            <AiOutlineCheck className="mr-2"/> Confirm
                         </Button>
                     </AlertDialogFooter>
                 </AlertDialogContent>
