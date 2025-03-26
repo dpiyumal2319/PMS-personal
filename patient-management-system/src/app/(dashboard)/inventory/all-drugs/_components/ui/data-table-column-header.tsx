034f257cf7a81717831e40bb70673bf744f9eb2e
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-03-26 17:47:54 +0530

    feat: remove unused imports and button from DataTableColumnHeader component

--- patient-management-system/src/app/(dashboard)/inventory/all-drugs/_components/ui/data-table-column-header.tsx
+++ patient-management-system/src/app/(dashboard)/inventory/all-drugs/_components/ui/data-table-column-header.tsx
@@ -1,13 +1,4 @@
 import {Column} from "@tanstack/react-table";
-import {ArrowUpDown} from "lucide-react";
-
-import {Button} from "@/components/ui/button";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
 
 interface DataTableColumnHeaderProps<TData, TValue> {
     column: Column<TData, TValue>;
@@ -21,15 +12,6 @@ export function DataTableColumnHeader<TData, TValue>({
         <div className="flex items-center space-x-2 justify-center">
             <span>{title}</span>
             <span className="inline-flex">
-        <Button
-            variant="ghost"
-            size="sm"
-            className="-ml-3 h-8 data-[state=open]:bg-accent"
-            aria-label={`Sort by ${title}`}
-            style={{height: "20px"}}
-        >
-          <ArrowUpDown className="h-3.5 w-3.5 text-gray-400"/>
-        </Button>
       </span>
         </div>
     );
