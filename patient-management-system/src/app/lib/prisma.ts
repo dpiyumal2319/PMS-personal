c67665ecc63f2de3cec64dc4c2a0739c04f489cb
dpiyumal2319 <w.piyumal2319@gmail.com>
2025-04-04 17:08:32 +0530

    feat: uncomment PrismaClient imports and extend functionality for remote database

--- patient-management-system/src/app/lib/prisma.ts
+++ patient-management-system/src/app/lib/prisma.ts
@@ -1,16 +1,16 @@
 // For remote database
 
-// import {PrismaClient} from '@prisma/client/edge'
-// import {withAccelerate} from '@prisma/extension-accelerate'
+import {PrismaClient} from '@prisma/client/edge'
+import {withAccelerate} from '@prisma/extension-accelerate'
 
-// export const prisma = new PrismaClient().$extends(withAccelerate())
+export const prisma = new PrismaClient().$extends(withAccelerate())
 
 
 // for local database
-import {PrismaClient} from '@prisma/client'
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+// import {PrismaClient} from '@prisma/client'
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-    globalForPrisma.prisma || new PrismaClient();
+// export const prisma =
+//     globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
